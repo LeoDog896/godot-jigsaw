645552e1dcf97982a4ed09b2e819405df03525bf
Tristan F <LeoDog896@hotmail.com>
2023-02-19 17:00:12 -0500

    feat: square ferret, puzzle piece generation

--- Puzzle.gd
+++ Puzzle.gd
@@ -5,19 +5,16 @@ export (Texture) var texture
 export (int) var rows = 0
 export (int) var cols = 0
 
-onready var puzzle_piece = preload("res://PuzzlePiece.tscn")
+var puzzle_piece := preload("res://PuzzlePiece.tscn")
 
 func _reverse_hinge(hinge: int) -> int:
-	if hinge == 1:
-		return 0
-	else:
-		return 1
+	return 0 if hinge == 1 else 1
 
 func _ready():
 	var pieces: Array = []
 	
 	for n in rows * cols:
-		var piece = puzzle_piece.instance()
+		var piece := puzzle_piece.instance()
 
 		# we need to pass down context info to the puzzle piece
 		# from the parent to keep it isolated
@@ -29,12 +26,25 @@ func _ready():
 		
 		piece.texture = texture
 		
-		# we use %s_neighbor as tracking variables on the Node,
+		# we use neighbor.* as tracking variables on the Node,
 		# the puzzle piece class doesn't have any use for them.
-		piece.neighbors.top = null if piece.row == 0 else pieces[n - cols * piece.row]
-		piece.neighbors.left = null if piece.col == 0 else pieces[n - 1]
-		piece.neighbors.right = null
-		piece.neighbors.bottom = null
+		piece.neighbors = {
+			top = null if piece.row == 0 else pieces[n - cols * piece.row],
+			left = null if piece.col == 0 else pieces[n - 1],
+			right = null,
+			bottom = null
+		}
+		
+		var image_size := Vector2(texture.get_width(), texture.get_height())
+		
+		piece.piece_scale = image_size / Vector2(cols, rows) * scale
+		
+		piece.hinge = {
+			top = 2 if piece.neighbors.top == null else _reverse_hinge(piece.neighbors.top.hinge.bottom),
+			left = 2 if piece.neighbors.left == null else _reverse_hinge(piece.neighbors.left.hinge.right),
+			right = 2 if piece.col == cols - 1 else randi() % 2,
+			bottom = 2 if piece.row == rows - 1 else randi() % 2
+		}
 		
 		if piece.neighbors.top != null:
 			piece.neighbors.top.neighbors.bottom = piece
@@ -42,12 +52,7 @@ func _ready():
 		if piece.neighbors.left != null:
 			piece.neighbors.left.neighbors.right = piece
 		
-		piece.piece_scale = Vector2(texture.get_width() / cols, texture.get_height() / rows) * scale
-		
-		piece.hinge.top = 2 if piece.neighbors.top == null else _reverse_hinge(piece.neighbors.top.hinge.bottom)
-		piece.hinge.left = 2 if piece.neighbors.left == null else _reverse_hinge(piece.neighbors.left.hinge.right)
-		piece.hinge.right = 2 if piece.col == cols - 1 else randi() % 2
-		piece.hinge.bottom = 2 if piece.row == rows - 1 else randi() % 2
+		piece.position = (piece.piece_scale * 2 * Vector2(piece.col, piece.row)) - image_size * scale
 		
 		pieces.append(piece)
 	
--- PuzzlePiece.gd
+++ PuzzlePiece.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
 enum HingeState {
-	EXTENDED,
-	CONTRACTED,
-	NONE
+	EXTENDED = 0,
+	CONTRACTED = 1,
+	NONE = 2
 }
 
 export (int) var rows
@@ -29,3 +29,58 @@ export(Dictionary) var hinge = {
 }
 
 export var piece_scale: Vector2
+onready var hinge_scale = piece_scale / 2
+
+onready var polygon := $Polygon2D
+
+func hinge(type: int, direction: Vector2) -> PoolVector2Array:
+	# this is technically a "right hinge", so we can rotate it to be whatever hinge we want
+	
+	var angle := direction.angle()
+	if type == HingeState.NONE:
+		return PoolVector2Array([ piece_scale.rotated(angle) ])
+	# since our puzzle piece is around (0, 0), we can use hinge_scale / 2 to define the hinge boundaries
+	return PoolVector2Array([
+		piece_scale.rotated(angle),
+		Vector2(piece_scale.x, hinge_scale.y/2).rotated(angle),
+		Vector2(piece_scale.x + hinge_scale.y * (1 if type == HingeState.EXTENDED else -1), hinge_scale.y/2).rotated(angle),
+		Vector2(piece_scale.x + hinge_scale.y * (1 if type == HingeState.EXTENDED else -1), -hinge_scale.y/2).rotated(angle),
+		Vector2(piece_scale.x, -hinge_scale.y/2).rotated(angle),
+	])
+
+func _ready() -> void:
+	polygon.polygon = (
+		hinge(hinge.right, Vector2.RIGHT)
+		+ hinge(hinge.top, Vector2.UP)
+		+ hinge(hinge.left, Vector2.LEFT)
+		+ hinge(hinge.bottom, Vector2.DOWN)
+	)
+	
+	polygon.texture = texture
+	
+	var localUV := []
+	
+	var IMAGE_WIDTH: int = texture.get_width() / cols
+	var IMAGE_HEIGHT: int = texture.get_height() / rows
+	
+	for vertex in polygon.polygon:
+		var normalized_vertex: Vector2 = (vertex / (piece_scale)) * (IMAGE_WIDTH / 2)
+		localUV.append(
+			normalized_vertex 
+			+ Vector2(
+				IMAGE_WIDTH / 2 + (IMAGE_WIDTH * col),
+				IMAGE_HEIGHT / 2 + (IMAGE_HEIGHT * row)
+			)
+		)
+	
+	polygon.uv = localUV
+
+# draws a black outline around the puzzle piece
+func _draw():
+	# get_polygon returns a clone, we dont want to reaccess it every time we get a polygon
+	var poly = polygon.get_polygon()
+	
+	var color = Color(0,0,0)
+	for i in range(1 , poly.size()):
+		draw_line(poly[i-1], poly[i], color, 5)
+	draw_line(poly[poly.size() - 1], poly[0], color, 5)
